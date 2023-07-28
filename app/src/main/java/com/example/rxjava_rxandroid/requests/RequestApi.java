75a280c006fdbe05a1c001c41a021b83eb25c290
biswa <biswaranjanbeheraqazmlp@gmail.com>
2023-07-28 10:34:59 +0530

    Transformation Operator FlatMap()

---
+++ app/src/main/java/com/example/rxjava_rxandroid/requests/RequestApi.java
@@ -0,0 +1,23 @@
+package com.example.rxjava_rxandroid.requests;
+
+
+
+import com.example.rxjava_rxandroid.models.Comment;
+import com.example.rxjava_rxandroid.models.Post;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface RequestApi {
+
+    @GET("posts")
+    Observable<List<Post>> getPosts();
+
+    @GET("posts/{id}/comments")
+    Observable<List<Comment>> getComments(
+            @Path("id") int id
+    );
+}
